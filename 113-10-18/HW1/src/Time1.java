e86a009441c335cfd024d3b1037c12dd749a491f
何思嫻 <a111223020@mail.shu.edu.tw>
2024-10-18 12:01:22 +0800

    113-10-18

---
+++ 113-10-18/HW1/src/Time1.java
@@ -0,0 +1,16 @@
+public class Time1 {
+    //時（hour)
+    //分（minute)
+    //秒(second)
+
+    //方法：建構子（設定時 分 秒的初始值，需檢查資料是否格式正確）
+
+    //方法：是需澳加入get/set
+
+    //方法：tick(時間增加一秒）
+
+    //方法：printUniversal(以24小時制輸出完整時間 14:05:32）
+
+    //方法：printStander(以12小時制輸出完整時間 2:05:65 PM
+    // )
+}
